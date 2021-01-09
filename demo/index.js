4611b4fe4f961a81ec757af119a248cba9829419
KaisarCode <kaisar@kaisarcode.com>
2021-01-09 03:51:37 -0300

    bpx package

--- demo/index.js
+++ demo/index.js
@@ -2,16 +2,10 @@ var bpx = require('../index');
 var fwt = require('kc-fwrite');
 var dir = __dirname;
 
-var conf = {
-    wrp: 'app',
-    pfx: 'bpx',
-    bps: [0, '768px'],
-};
-
 bpx({
     src: `${dir}/app.css`,
     out: `${dir}/out.css`,
-    cfg: conf
 },function(css){
     
+    console.log('CSS compiled');
 });
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var css = require('css');
+var mrg = require('kc-obj-merge');
 var frd = require('kc-fread');
 var fwt = require('kc-fwrite');
+var stm = require('kc-strmin');
+var rmc = require('kc-rmcomm');
 var tgf = require('kc-tagfun');
-var rpl = require('kc-strrepl');
+var run = require('asbox');
+var t2j = require('t2js');
 var dir = __dirname;
 
 module.exports = function(opt, cb){
@@ -18,23 +21,21 @@ module.exports = function(opt, cb){
     
     // Parse templates
     str = tgf(str, '@{', '}', function(c){
-        c = c.replace(/__BPXCORE/g, dir+'/src');
+        c = c.replace(/__BPX/g, dir+'/src');
         return frd(c);
+    }); str = t2j(str);
+    
+    // Load configs
+    var cfg = frd(dir+'/src/conf.json');
+    cfg = rmc(cfg);
+    cfg = JSON.parse(cfg);
+    cfg = mrg(cfg, opt.cfg);
+    
+    // Build CSS
+    run(str, cfg, function(str){
+        str = rmc(str);
+        str = stm(str);
+        if (opt.out) fwt(opt.out, str);
+        cb(str);
     });
-    
-    var obj = css.parse(str, { silent: true });
-    obj.stylesheet.rules.forEach(function(r) {
-        if (r.type == 'rule' && opt.cfg.pfx) {
-            r.selectors.map(function(a, i) {
-                a = rpl('.', `.${opt.cfg.pfx}-`, a);
-                r.selectors[i] = a;
-            });
-        }
-    });
-    
-    obj = JSON.stringify(obj, null, 4);
-    if (opt.out) fwt(opt.out+'.json', obj);
-    
-    if (opt.out) fwt(opt.out, str);
-    cb(str);
 }
